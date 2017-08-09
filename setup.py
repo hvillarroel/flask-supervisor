9f48265255828bf21e679c1c966d06a2c830cd8a
hvillarroel <hvillarroel@synaptic.cl>
2017-08-09 17:31:25 -0400

    Update

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ setup(name="flask-supervisor",
       version="1.0",
       description="",
       author="Héctor Villarroel",
-      author_email='',
+      author_email='hectorvillarroel.p@gmail.com',
       url='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          ["Flask", "Flask-admin"]
       ],
       )
